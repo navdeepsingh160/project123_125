b4da76fcbd6e09b0731723893135b3c2c90892dc
navdeepsingh160 <118738353+navdeepsingh160@users.noreply.github.com>
2023-09-19 16:14:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+noseX=0;
+noseY=0;
 
+function preload() {
+mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");   
 }
 
 function setup() {
@@ -19,13 +22,16 @@ function gotPoses(results){
     if (results.length > 0)
     {
         console.log(results);
+        noseX=results[0].pose.nose.x-40;
+        noseY=results[0].pose.nose.y;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
 }
 
 function draw() {
-    image (video, 0, 0, 300, 300);
+    image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 80, 35);
 
 }
 
